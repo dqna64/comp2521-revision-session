e4fdf1f49f3b0b0fa14a6428b64daf1b4f31bc6f
Allynixtor <bunchymovie123@gmail.com>
2023-07-29 22:12:30 +1000

    update compilation commands

--- autotest.sh
+++ autotest.sh
@@ -4,9 +4,9 @@ parameters="
 	default_compilers = {'c' : [['clang', '-Werror', '-std=gnu11', '-g', '-lm']]}
 	upload_url = https://example.com/autotest.cgi
 "
-target="$(readlink -f "$0")"
+target="$(readlink -n "$0")"
 targetDir="$(dirname "$target")"
 
-exec "/usr/local/bin/autotest/autotest.py" --exercise_directory "${targetDir}/problems" --parameters "$parameters" "$@"
+exec python3 "/usr/local/bin/autotest/autotest.py" --exercise_directory "${targetDir}/problems" --parameters "$parameters" "$@"
 
 
